7f067e37612c768fdfd0ad6620e14b86da48859c
Michael Killough <michaeljkillough@gmail.com>
2016-07-15 22:56:30 +0100

    Use dj-database-url so that we use Postgres on Heroku.

--- workaholic/settings.py
+++ workaholic/settings.py
@@ -3,6 +3,8 @@
 
 import os
 
+import dj_database_url
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
@@ -59,9 +61,8 @@ TEMPLATES = [
 ]
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(
+    default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+    conn_max_age=600
+)
