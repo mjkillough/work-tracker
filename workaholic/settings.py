81964e171a75ed0d819088a039bf0ab83c445ae0
Michael Killough <michaeljkillough@gmail.com>
2016-07-15 23:48:47 +0100

    Add .zvxrl.co.uk subdomain too.
    Will need to wait for CloudFare to kick into action before we
    can use it. We need HTTPS for the service workers.

--- workaholic/settings.py
+++ workaholic/settings.py
@@ -9,7 +9,10 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 DEBUG = os.environ.get('DJANGO_DEBUG') == 'true'
-ALLOWED_HOSTS = ['workaholic.herokuapp.com']
+ALLOWED_HOSTS = [
+    'workaholic.zvxrl.co.uk',
+    'workaholic.herokuapp.com'
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
