2201f380a19e5ba25937cbff0f7f553699fa96f8
Michael Killough <michaeljkillough@gmail.com>
2016-07-16 17:12:17 +0100

    Fix imports.
    `django.contrib.auth` doesn't import everything one might hope...

--- workaholic/forms.py
+++ workaholic/forms.py
@@ -2,13 +2,14 @@
 # encoding: utf-8
 
 from django import forms
-from django.contrib import auth
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
-class SignupForm(auth.forms.UserCreationForm):
+class SignupForm(UserCreationForm):
 
     class Meta:
-        model = auth.models.User
+        model = User
         fields = ("username", "email", "password1", "password2")
 
     email = forms.EmailField(required=True)
